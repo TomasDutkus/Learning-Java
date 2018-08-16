c6abed991b3bdd42b5f93e070896f25d55d3ffb3
Tomas Dutkus <tomasdutkus@gmail.com>
2018-08-16 15:36:51 +0300

    Homework

---
+++ src/lt/baltictalents/lecture12/a/PointMain.java
@@ -0,0 +1,18 @@
+package lt.baltictalents.lecture12.a;
+
+public class PointMain {
+
+    public static void main(String[] args) {
+
+        Point point = new Point(1, 2);
+        int x = point.getX();
+        int y = point.getY();
+
+        point.setX(x - y);
+        point.setY(y - x);
+
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+
+    }
+}
---
+++ src/lt/baltictalents/lecture12/b/Arrays.java
@@ -0,0 +1,16 @@
+package lt.baltictalents.lecture12.b;
+
+import java.util.stream.IntStream;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] a = {5, 6, 10, 15, 8, 4};
+        int[] b = {8, 5, 3};
+
+        int sum = IntStream.of(a).sum();
+        int sum2 = IntStream.of(b).sum();
+
+        System.out.println(sum - sum2);
+
+    }
+}
