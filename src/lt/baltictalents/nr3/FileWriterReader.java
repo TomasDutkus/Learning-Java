1681d389ae67ec92a8f08e3a9d63a76c16e2da4f
Tomas Dutkus <tomasdutkus@gmail.com>
2018-08-28 14:24:56 +0300

    Updated to write into file and read from it in a column

--- src/lt/baltictalents/nr3/FileWriterReader.java
+++ src/lt/baltictalents/nr3/FileWriterReader.java
@@ -3,22 +3,34 @@ package lt.baltictalents.nr3;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileWriterReader {
     private static final String filename = "resources/Text.txt";
+    public static final String DELIMITER = "[\\s]*,[\\s]*";
     public static void main(String[] args) throws IOException {
 
         printToFile(filename);
+        Path path = Paths.get("resources/Text.txt");
+        Files.lines(path).map(line -> line.trim().split(DELIMITER))
+                .forEach(arr -> {
+                    for (String a : arr) {
+                        System.out.println(a);
+                    }
+                });
+        System.out.println("--------------------------------");
 
     }
 
     private static void printToFile(String fileName) throws FileNotFoundException {
         PrintWriter writer = new PrintWriter(fileName);
         writer.printf("vienas,du,  trys , nulis%n");
-        writer.println("|vienas|");
-        writer.println("|du|");
-        writer.println("|trys|");
-        writer.println("|nulis|");
+/*        writer.println("vienas");
+        writer.println("du");
+        writer.println("trys");
+        writer.println("nulis"); */
         writer.close();
     }
 }
